e4429b3a9100f0d968704fd5143b184287d3a678
AlexFabiany <alexfabianyleao@yahoo.com.br>
2021-06-11 19:01:59 -0400

    Added FirstController, Model Person and View Info

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,32 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public IActionResult Hello()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.Name = "Alex";
+            person.Age = 49;
+            person.Location = "Manaus-AM-Brazil";
+            return View(person);
+        }
+    }
+}
